d8f14236b594159d9ed861aa4572d6f112febbd2
Sardor <41294422+SardorShoh@users.noreply.github.com>
2021-06-07 23:19:16 +0500

    commit

--- db/models.go
+++ db/models.go
@@ -17,6 +17,14 @@ type Test struct {
 	ID      uint64         `json:"id"`
 	Title   string         `json:"title"`
 	Answers types.JSONText `json:"answers"`
+	Correct string         `json:"correct,omitempty"`
+	Type    uint           `json:"type,omitempty"`
 	Created time.Time      `json:"created,omitempty"`
 	Updated time.Time      `json:"updated,omitempty"`
+}
+
+//User -> Foydalanuvchi strukturasi
+type User struct {
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
 }
